005c10c2cc76fea52152934bedbc13335faa0eed
Lyn Levenick <lyn.levenick@gmail.com>
2018-08-09 15:42:22 -0700

    Formatting

--- install.sh
+++ install.sh
@@ -17,42 +17,42 @@ main() {
         ln -si "${script_dir}/Brewfile" "${HOME}/.Brewfile" || true
         ln -si "${script_dir}/gitconfig" "${HOME}/.gitconfig" || true
         ln -si "${script_dir}/gitignore" "${HOME}/.gitignore" || true
-    fi
 
-    echo -n 'Install all base packages (Y/n) => '; read answer
-    if [ "$answer" != 'n' ] && [ "$answer" != 'N' ] ; then
-        sudo chown -R "${USER}:admin" /usr/local
-        /usr/bin/env ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        echo -n 'Install packages (Y/n) => '; read answer
+        if [ "$answer" != 'n' ] && [ "$answer" != 'N' ] ; then
+            sudo chown -R "${USER}:admin" /usr/local
+            /usr/bin/env ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-        brew doctor
-        brew update
+            brew doctor
+            brew update
 
-        brew tap homebrew/bundle
-        brew bundle install --global
-    fi
+            brew tap homebrew/bundle
+            brew bundle install --global
 
-    echo -n 'Switch user shell to fish (Y/n) => '; read answer
-    if [ "$answer" != 'n' ] && [ "$answer" != 'N' ] ; then
-        echo '/usr/local/bin/fish' | sudo tee -a /etc/shells >/dev/null
-        sudo chsh -u "$USER" -s '/usr/local/bin/fish'
-    fi
+            echo -n 'Switch user shell to fish (Y/n) => '; read answer
+            if [ "$answer" != 'n' ] && [ "$answer" != 'N' ] ; then
+                echo '/usr/local/bin/fish' | sudo tee -a /etc/shells >/dev/null
+                sudo chsh -u "$USER" -s '/usr/local/bin/fish'
+            fi
 
-    echo -n 'Install vscode settings (Y/n) => '; read answer
-    if [ "$answer" != 'n' ] && [ "$answer" != 'N' ] ; then
-        mkdir -p "${HOME}/Library/Application Support/Code/User"
-        ln -si "${script_dir}/vscode/settings.json" "${HOME}/Library/Application Support/Code/User/settings.json"
-    fi
+            echo -n 'Install vscode settings (Y/n) => '; read answer
+            if [ "$answer" != 'n' ] && [ "$answer" != 'N' ] ; then
+                mkdir -p "${HOME}/Library/Application Support/Code/User"
+                ln -si "${script_dir}/vscode/settings.json" "${HOME}/Library/Application Support/Code/User/settings.json"
 
-    echo -n 'Install vscode extensions (Y/n) => '; read answer
-    if [ "$answer" != 'n' ] && [ "$answer" != 'N' ] ; then
-        code --install-extension 'editorconfig.editorconfig'
-        code --install-extension 'eamodio.gitlens'
-        code --install-extension 'zhuangtongfa.material-theme'
-        code --install-extension 'robertohuertasm.vscode-icons'
+                echo -n 'Install vscode extensions (Y/n) => '; read answer
+                if [ "$answer" != 'n' ] && [ "$answer" != 'N' ] ; then
+                    code --install-extension 'editorconfig.editorconfig'
+                    code --install-extension 'eamodio.gitlens'
+                    code --install-extension 'zhuangtongfa.material-theme'
+                    code --install-extension 'robertohuertasm.vscode-icons'
 
-        # Ruby
-        code --install-extension 'rebornix.ruby'
-        code --install-extension 'karunamurti.haml'
+                    # Ruby
+                    code --install-extension 'rebornix.ruby'
+                    code --install-extension 'karunamurti.haml'
+                fi
+            fi
+        fi
     fi
 }
 
