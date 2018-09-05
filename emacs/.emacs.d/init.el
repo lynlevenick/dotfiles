1a532c3cc7e1bbd5de8610911f4cedf1f203cbfe
Lyn Levenick <lyn.levenick@gmail.com>
2018-09-05 03:16:07 -0700

    Lazily load htmlize and ox-reveal

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -80,10 +80,12 @@ point reaches the beginning of end of the buffer, stop there."
 ;;;; Major Modes
 (def-package! haml-mode
   :mode "\\.haml\\'")
-(def-package! htmlize)
+(def-package! htmlize
+  :after org)
 (def-package! org
   :mode ("\\.org\\'" . org-mode))
-(def-package! ox-reveal)
+(def-package! ox-reveal
+  :after org)
 (setf ruby-align-chained-calls t)
 (def-package! rust-mode
   :mode "\\.rs\\'")
