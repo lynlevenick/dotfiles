c46f516a49e5ef0b4fa4b04bfde088dcebd4013d
Lyn Levenick <lyn.levenick@gmail.com>
2018-09-16 15:17:47 -0700

    no wait go back

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -127,8 +127,8 @@ through to `use-package'."
 (push '(width . 100) default-frame-alist)
 (push '(ns . ((ns-transparent-titlebar . t))) window-system-default-frame-alist)
 
-(def-package! atom-one-dark-theme
-  :config (load-theme 'atom-one-dark t))
+(def-package! srcery-theme
+  :config (load-theme 'srcery t))
 (def-package! minions
   :config (minions-mode 1)
   :custom (minions-mode-line-lighter "\u2026"))
