60e1a68294548b5bdb259399a51f4c4eaae24ad4
Lyn Levenick <lyn.levenick@gmail.com>
2019-10-28 17:13:15 -0700

    Handle ri in rvm

--- sh/.config/sh/rvm.sh
+++ sh/.config/sh/rvm.sh
@@ -2,11 +2,12 @@
 
 if test -r "${HOME}/.rvm/scripts/rvm"; then
 	if test -t 1; then
-		alias bundle='unalias bundle gem irb ruby rvm && . "${HOME}/.rvm/scripts/rvm" && bundle'
-		alias gem='unalias bundle gem irb ruby rvm && . "${HOME}/.rvm/scripts/rvm" && gem'
-		alias irb='unalias bundle gem irb ruby rvm && . "${HOME}/.rvm/scripts/rvm" && irb'
-		alias ruby='unalias bundle gem irb ruby rvm && . "${HOME}/.rvm/scripts/rvm" && ruby'
-		alias rvm='unalias bundle gem irb ruby rvm && . "${HOME}/.rvm/scripts/rvm" && rvm'
+		alias bundle='unalias bundle gem irb ri ruby rvm && . "${HOME}/.rvm/scripts/rvm" && bundle'
+		alias gem='unalias bundle gem irb ri ruby rvm && . "${HOME}/.rvm/scripts/rvm" && gem'
+		alias irb='unalias bundle gem irb ri ruby rvm && . "${HOME}/.rvm/scripts/rvm" && irb'
+		alias ri='unalias bundle gem irb ri ruby rvm && . "${HOME}/.rvm/scripts/rvm" && ri'
+		alias ruby='unalias bundle gem irb ri ruby rvm && . "${HOME}/.rvm/scripts/rvm" && ruby'
+		alias rvm='unalias bundle gem irb ri ruby rvm && . "${HOME}/.rvm/scripts/rvm" && rvm'
 	else
 		. "${HOME}/.rvm/scripts/rvm"
 	fi
