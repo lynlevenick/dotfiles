11d4df9f989f24227b979c02f53f99b51989064e
Ashlynn Levenick <lyn.levenick@gmail.com>
2019-12-06 02:01:44 +0000

    Remove ripgrep dependency from zsql

--- sh/.config/sh-interactive/zsql.sh
+++ sh/.config/sh-interactive/zsql.sh
@@ -7,7 +7,7 @@ if command -v sqlite3 >/dev/null; then
 		mkdir -p "$(dirname "$__zsql_cache")"
 
 		sqlite3 "$__zsql_cache" <<SQL >/dev/null
-CREATE TABLE dirs (dir TEXT NOT NULL, frecency INTEGER NOT NULL);
+CREATE TABLE dirs (dir TEXT NOT NULL, frecency INTEGER NOT NULL DEFAULT 1);
 CREATE UNIQUE INDEX index_by_dir ON dirs (dir);
 CREATE INDEX index_by_frecency_and_dir ON dirs (frecency, dir);
 CREATE TRIGGER trigger_on_update_forget
@@ -23,13 +23,34 @@ SQL
 	PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND;}__zsql_add_async"
 fi
 
+# Escape a string for sqlite, by turning single quotes into two single quotes
+__zsql_escape() {
+	sed 's/'\''/'\'\''/g'
+}
+
+# Consume standard input, printing until the first NUL byte
+__zsql_read_to_nul() {
+	printf '%b' $(od -v -b | sed -n ':loop
+s/^[^ ]*//
+/ 000/b end
+s/  */ \\/g
+p
+n
+b loop
+:end
+s/ *000.*//
+s/  */ \\/g
+p
+q')
+}
+
 __zsql_add() {
-	__zsql_escaped="$(printf '%s$' "$1" | sed 's/'\''/'\'\''/g')"
+	__zsql_escaped="$(printf '%s$' "$1" | __zsql_escape)"
 
 	sqlite3 "$__zsql_cache" <<SQL
 .timeout 100
-INSERT INTO dirs (dir, frecency)
-	VALUES ('${__zsql_escaped%?}', 1)
+INSERT INTO dirs (dir)
+	VALUES ('${__zsql_escaped%?}')
 	ON CONFLICT (dir) DO UPDATE SET
 	frecency = frecency + excluded.frecency;
 SQL
@@ -39,12 +60,12 @@ __zsql_add_async() {
 }
 
 __zsql_cd() {
-	CDPATH='' cd -- "${1%??}" 2>/dev/null || printf 'fatal: cd: `%s'\'' not found\n' "${1%??}"
+	CDPATH='' cd -- "${1%?}" 2>/dev/null || printf 'fatal: cd: `%s'\'' not found\n' "${1%?}"
 }
 
 __zsql_forget() {
 	while :; do
-		printf 'Remove `%s'\''? [Yn] ' "${1%??}"
+		printf 'Remove `%s'\''? [Yn] ' "${1%?}"
 		IFS='' read -r __zsql_yn
 		case "$__zsql_yn" in
 			''|y|Y)
@@ -56,7 +77,7 @@ __zsql_forget() {
 		esac
 	done
 
-	__zsql_escaped="$(printf '%s$' "${1%??}" | sed 's/'\''/'\'\''/g')"
+	__zsql_escaped="$(printf '%s$' "${1%?}" | __zsql_escape)"
 	sqlite3 "$__zsql_cache" <<SQL
 .timeout 100
 DELETE FROM dirs WHERE dir = '${__zsql_escaped%?}';
@@ -77,7 +98,7 @@ z() {
 				shift
 				break ;;
 			*)
-				break ;;
+				break
 		esac
 
 		shift
@@ -88,10 +109,10 @@ z() {
 	fi
 
 	if test -n "$*"; then
-		__zsql_escaped_pwd="$(printf '%s$' "$(pwd)" | sed 's/'\''/'\'\''/g')"
+		__zsql_escaped_pwd="$(printf '%s$' "$(pwd)" | __zsql_escape)"
 		__zsql_filtered_search="$(printf '%s$' "$*" | sed -e 's/\(.\)/%\1/g' -e 's/'\''/'\'\''/g')"
 		__zsql_selection="$(
-			sqlite3 "$__zsql_cache" <<SQL | xargs printf '%b\0' | fzf --read0 --print0 --filter="$*" | rg --text --multiline --only-matching --max-count=1 '(?-u)^([^\x00]+)' && printf '$'
+			sqlite3 "$__zsql_cache" <<SQL | xargs printf '%b\0' 2>/dev/null | fzf --read0 --print0 --filter="$*" | __zsql_read_to_nul && printf '$'
 .mode tcl
 .timeout 100
 ${__zsql_case_sensitive:+PRAGMA case_sensitive_like = ON;}
@@ -103,7 +124,7 @@ SQL
 		)"
 	else
 		__zsql_selection="$(
-			sqlite3 "$__zsql_cache" <<SQL | xargs printf '%b\0' | fzf-tmux --read0 --select-1 --bind='?:toggle-preview' --preview='env CLICOLOR_FORCE=1 ls -G -- {}' --preview-window=hidden && printf '$'
+			sqlite3 "$__zsql_cache" <<SQL | xargs printf '%b\0' 2>/dev/null | fzf-tmux --read0 --select-1 --bind='?:toggle-preview' --preview='env CLICOLOR_FORCE=1 ls -G -- {}' --preview-window=hidden && printf '$'
 .mode tcl
 .timeout 100
 SELECT dir FROM dirs ORDER BY frecency DESC;
@@ -112,13 +133,12 @@ SQL
 	fi
 
 	__zsql_exitstatus="$?"
-
 	if test "0$__zsql_exitstatus" -eq 130; then
-		: # User probably manually exited with C-C, C-D, or C-G
+		return 0 # User probably manually exited with C-C, C-D, or C-G
 	elif test -z "$__zsql_selection" || test "$__zsql_selection" = '$'; then
 		printf 'fatal: z: Not in history\n'
-		false
-	elif test -n "${__zsql_selection%??}"; then
+		return 1
+	elif test -n "${__zsql_selection%?}"; then
 		"$__zsql_action" "$__zsql_selection"
 	fi
 }
